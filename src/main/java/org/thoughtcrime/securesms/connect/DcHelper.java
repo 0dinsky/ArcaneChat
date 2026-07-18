ba994f4dc0207a4a720e76a7cfedb822f377537e
0dinsky <alex.v.ledenev234@gmail.com>
2026-07-18 15:54:34 +0300

    Fix logo

--- src/main/java/org/thoughtcrime/securesms/connect/DcHelper.java
+++ src/main/java/org/thoughtcrime/securesms/connect/DcHelper.java
@@ -568,7 +568,7 @@ public class DcHelper {
       if (lastError != null && !lastError.isEmpty()) {
         Log.w(TAG, "Opening account failed, trying to share error: " + lastError);
 
-        String subject = "Delta Chat failed to update";
+        String subject = "Arcane Chat failed to update";
         String email = "delta@merlinux.eu";
 
         new AlertDialog.Builder(context)
